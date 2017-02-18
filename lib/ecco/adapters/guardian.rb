1d9f37b3047ad4699f3dfae9ef4466ada7553a35
grekuna <gregor.martius@gmail.com>
2017-02-18 14:06:36 +0100

    bug fix, code crashed if a video was linked with the main article

--- lib/ecco/adapters/guardian.rb
+++ lib/ecco/adapters/guardian.rb
@@ -4,8 +4,7 @@ module Ecco
 
     def initialize
       @root_uri = 'https://www.theguardian.com/international'
-      @article_block =
-      get_html(root_uri).css('.fc-item__content')
+      @article_block = get_html(root_uri).css('.fc-item__content')
       @page_block = get_html(root_uri)
     end
 
@@ -14,7 +13,7 @@ module Ecco
         headline: article_block.css('.fc-item__header').first.inner_text,
         description: article_block.css('.fc-item__standfirst').first.content,
         link: URI.parse(root_uri + page_block.css('.u-faux-block-link__overlay').first.attr('href')),
-        image_url: URI.parse(page_block.css('.fc-item__media-wrapper source').first.attr('srcset'))
+        image_url: URI.parse(page_block.css('.fc-item__media-wrapper picture source').first.attr('srcset'))
       )
     end
   end
