b2042dfd0ce746fafc1c4387817d86e321206d36
grekuna <gregor.martius@gmail.com>
2017-02-18 14:08:24 +0100

    refactor 1

--- lib/ecco/adapters/guardian.rb
+++ lib/ecco/adapters/guardian.rb
@@ -1,19 +1,17 @@
 module Ecco
   class GuardianAdapter < Adapter
-    attr_reader :page_block
 
     def initialize
       @root_uri = 'https://www.theguardian.com/international'
-      @article_block = get_html(root_uri).css('.fc-item__content')
-      @page_block = get_html(root_uri)
+      @article_block = get_html(root_uri)
     end
 
     def data
       SiteData.new(
-        headline: article_block.css('.fc-item__header').first.inner_text,
-        description: article_block.css('.fc-item__standfirst').first.content,
-        link: URI.parse(root_uri + page_block.css('.u-faux-block-link__overlay').first.attr('href')),
-        image_url: URI.parse(page_block.css('.fc-item__media-wrapper picture source').first.attr('srcset'))
+        headline: article_block.css('.fc-item__content .fc-item__header').first.inner_text,
+        description: article_block.css('.fc-item__content .fc-item__standfirst').first.content,
+        link: URI.parse(root_uri + article_block.css('.u-faux-block-link__overlay').first.attr('href')),
+        image_url: URI.parse(article_block.css('.fc-item__media-wrapper picture source').first.attr('srcset'))
       )
     end
   end
