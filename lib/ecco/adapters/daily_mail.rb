ca7ce8e5fbd5feee41be399795ee35664efadf24
Rodrigo Haenggi <rodrigo@codegestalt.com>
2017-02-20 14:56:01 +0100

    fix the daily mail adapter

--- lib/ecco/adapters/daily_mail.rb
+++ lib/ecco/adapters/daily_mail.rb
@@ -10,8 +10,8 @@ module Ecco
       SiteData.new(
         headline: article_block.css('.linkro-darkred > a').text,
         description: article_block.css('.articletext > div > p').first.inner_text,
-        link: URI.parse(root_uri + article_block.css('.articletext a').first['href']),
-        image_url: URI.parse(article_block.css('.articletext > a > img').first['data-src'])
+        link: URI.parse(root_uri + article_block.css('.article a').first.attr('href')),
+        image_url: URI.parse(article_block.css('.article img').first.attr('data-src'))
       )
     end
   end
