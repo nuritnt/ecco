d4999aeb81a0a1fb157d5a68155d5042d9b7f199
Bootcamp <bootcamp@Sab.local>
2017-02-20 14:52:46 +0100

    uncomment old test and add http to elpaiss

--- test/test_crawler.rb
+++ test/test_crawler.rb
@@ -7,27 +7,27 @@ require_relative '../lib/ecco'
 
 module Ecco
   class CrawlerTest < Minitest::Test
-    # def test_le_monde
-    #   crawler = Crawler.new(:lemonde)
-    #   site_data = crawler.crawl
-    #
-    #   assert_kind_of Ecco::SiteData, site_data
-    #   assert_kind_of String, site_data.headline
-    #   assert_kind_of String, site_data.description
-    #   assert_kind_of URI, site_data.link
-    #   assert_kind_of URI, site_data.image_url
-    # end
-    #
-    # def test_nzz
-    #   crawler = Crawler.new(:nzz)
-    #   site_data = crawler.crawl
-    #
-    #   assert_kind_of Ecco::SiteData, site_data
-    #   assert_kind_of String, site_data.headline
-    #   assert_kind_of String, site_data.description
-    #   assert_kind_of URI, site_data.link
-    #   assert_kind_of URI, site_data.image_url
-    # end
+    def test_le_monde
+      crawler = Crawler.new(:lemonde)
+      site_data = crawler.crawl
+
+      assert_kind_of Ecco::SiteData, site_data
+      assert_kind_of String, site_data.headline
+      assert_kind_of String, site_data.description
+      assert_kind_of URI, site_data.link
+      assert_kind_of URI, site_data.image_url
+    end
+
+    def test_nzz
+      crawler = Crawler.new(:nzz)
+      site_data = crawler.crawl
+
+      assert_kind_of Ecco::SiteData, site_data
+      assert_kind_of String, site_data.headline
+      assert_kind_of String, site_data.description
+      assert_kind_of URI, site_data.link
+      assert_kind_of URI, site_data.image_url
+    end
 
     def test_elpais
       crawler = Crawler.new(:elpais)
